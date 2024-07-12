2a534e2e1f255536771da854b69158b6ae141b48
SujahathMSM <sujahathmhmd@Gmail.com>
2024-07-12 17:02:00 +0530

    Added a default route

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { saveUser, getUser, updateLocation,getAllUserData, getAnyData} = require('../Controllers/userController');
 const User = require('../Models/user');
 
+router.get('/', (req, res) => res.send("App is live now"))
 router.post('/save', saveUser);
 router.get('/retrieve', getUser )
 router.put('/update-location', updateLocation )
